75cca1df5a0e3581eaf6d64a2acbac00a081ac8c
ustavoteles <telesgustavo.dev@gmail.com>
2025-01-28 08:41:36 -0300

    :sparkles: Recurso Categoria

---
+++ src/categoria/entities/categoria.entity.ts
@@ -0,0 +1,15 @@
+import { IsNotEmpty } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'tb_categorias' })
+export class Categoria {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @IsNotEmpty()
+  @Column({ length: 50, nullable: false })
+  nomeCategoria: string;
+
+  //   @OneToMany(() => Produto, (produto) => produto.categoria)
+  //   produto: Produto[];
+}
